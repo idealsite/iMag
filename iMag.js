ea7931be5439a3b8328bc0f379ea82dcfd808e09
idealsite <eduardo@idealsite.it>
2011-01-09 01:38:57 +0100

    Going on :P

--- iMag.js
+++ iMag.js
@@ -1,10 +1,36 @@
 var iMag = [];
 var arrowWidth = 48;
 var arrowHeight = 48;
-var speed = 1.5;
+var globalSpeed = 1.5;
 
+function moveLeft(i) {
 
+};
+function moveUp(i) {
 
-$(document).ready(function(){
+};
+function moveRight(i) {
 
+};
+function moveDown(i) {
+
+};
+
+$(document).ready(function() {
+  var number = $(".iMag").size();
+  for (var i = 0; i < number; i++) {
+    var currentDiv = $(".iMag").eq(i);
+    var currentImage = $(".iMag > img").eq(i);
+    iMag[i] = {};
+    iMag[i].divWidth = currentDiv.width();
+    iMag[i].divHeight = currentDiv.height();
+    iMag[i].imageWidth = currentImage.width();
+    iMag[i].imageHeight = currentImage.height();
+    iMag[i].xMin = iMag[i].divWidth - iMag[i].imageWidth;
+    iMag[i].yMin = iMag[i].divHeight - iMag[i].imageHeight;
+    iMag[i].xMax = 0;
+    iMag[i].yMax = 0;
+    iMag[i].xCenter = iMag[i].xMin/2;
+    iMag[i].yCenter = iMag[i].yMin/2;
+  };
 });
