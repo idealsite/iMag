9c076eab7663e430b6f706f8f03326bf1a38f4ba
idealsite <eduardo@idealsite.it>
2011-01-10 21:16:13 +0100

    ok

--- iMag.js
+++ iMag.js
@@ -3,54 +3,43 @@ var controlsWidth = 48;
 var controlsHeight = 48;
 var globalSpeed = 1.5;
 
-function moveLeft(i) {
-
-};
-function moveUp(i) {
-
-};
-function moveRight(i) {
-
-};
-function moveDown(i) {
-
-};
-
 $(document).ready(function() {
   var number = $(".iMag").size();
   for (var i = 0; i < number; i++) {
-    var currentDiv = $(".iMag").eq(i);
-    var currentImage = $(".iMag > img").eq(i);
-    currentImage.addClass("iMag_image");
+    var div = $(".iMag").eq(i);
+    var image = $(".iMag").eq(i).find("img").eq(0);
+    image.addClass("iMag_image");
     iMag[i] = {};
-    iMag[i].divWidth = currentDiv.width();
-    iMag[i].divHeight = currentDiv.height();
-    iMag[i].imageWidth = currentImage.width();
-    iMag[i].imageHeight = currentImage.height();
+    iMag[i].div = div;
+    iMag[i].image = image;
+    iMag[i].divWidth = div.width();
+    iMag[i].divHeight = div.height();
+    iMag[i].imageWidth = image.width();
+    iMag[i].imageHeight = image.height();
     iMag[i].xMin = iMag[i].divWidth - iMag[i].imageWidth;
-    iMag[i].yMin = iMag[i].divHeight - iMag[i].imageHeight;
     iMag[i].xMax = 0;
-    iMag[i].yMax = 0;
     iMag[i].xCenter = iMag[i].xMin/2;
+    iMag[i].yMin = iMag[i].divHeight - iMag[i].imageHeight;
+    iMag[i].yMax = 0;
     iMag[i].yCenter = iMag[i].yMin/2;
     iMag[i].controlsX = (iMag[i].divWidth - controlsWidth)/2;
     iMag[i].controlsY = (iMag[i].divHeight - controlsHeight)/2;
-    if (currentDiv.hasClass("centerX")) {
-      currentImage.css("left", iMag[i].xCenter);
+    if (div.hasClass("centerX")) {
+      image.css("left", iMag[i].xCenter);
     };
-    if (currentDiv.hasClass("centerY")) {
-      currentImage.css("top", iMag[i].yCenter);
+    if (div.hasClass("centerY")) {
+      image.css("top", iMag[i].yCenter);
     };
-    if (currentDiv.hasClass("centered")) {
-      currentImage.css({
+    if (div.hasClass("centered")) {
+      image.css({
         "left": iMag[i].xCenter,
         "top": iMag[i].yCenter
       });
     };
-    var leftControl = '<img src="images/arrow_left.png" class="iMag_control" style="left:0px; top:'+iMag[i].controlsY+'px;" />';
-    var upControl = '<img src="images/arrow_up.png" class="iMag_control" style="left:'+iMag[i].controlsX+'px; top:0px;" />';
-    var rightControl = '<img src="images/arrow_right.png" class="iMag_control" style="right:0px; top:'+iMag[i].controlsY+'px;" />';
-    var downControl = '<img src="images/arrow_down.png" class="iMag_control" style="left:'+iMag[i].controlsX+'px; bottom:0px;" />';
-    currentImage.after(leftControl, upControl, rightControl, downControl);
+    iMag[i].leftControl = $('<img src="images/arrow_left.png" class="iMag_control" style="left:0px; top:'+iMag[i].controlsY+'px;" />');
+    iMag[i].upControl = $('<img src="images/arrow_up.png" class="iMag_control" style="left:'+iMag[i].controlsX+'px; top:0px;" />');
+    iMag[i].rightControl = $('<img src="images/arrow_right.png" class="iMag_control" style="right:0px; top:'+iMag[i].controlsY+'px;" />');
+    iMag[i].downControl = $('<img src="images/arrow_down.png" class="iMag_control" style="left:'+iMag[i].controlsX+'px; bottom:0px;" />');
+    image.after(iMag[i].leftControl, iMag[i].upControl, iMag[i].rightControl, iMag[i].downControl);
   };
 });
