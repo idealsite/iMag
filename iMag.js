cee67d1ecf87ff483f7a203b1de7c6dab032cdfc
idealsite <eduardo@idealsite.it>
2011-01-10 22:09:28 +0100

    Near to the end

--- iMag.js
+++ iMag.js
@@ -1,7 +1,50 @@
 var iMag = [];
-var controlsWidth = 48;
+//
+//Variables you can change to personalize your iMag
+var controlsWidth = 48; //Change this if you use different icons for the controllers
 var controlsHeight = 48;
-var globalSpeed = 1.5;
+var globalSpeed = 5; //This is the speed assumed in case you don't define one for an iMag, higher numbers return lower speed
+iMag[0] = {}; //Set up the object for the iMag you want to assign speed
+iMag[0].speed = 1; //This way you can set every iMag's speed, higher numbers return lower speed
+//iMag[1].speed = 10;
+//
+function moveRight(caller) {
+  var i = $(caller).data("index");
+  var speed = (iMag[i].speed) ? iMag[i].speed : globalSpeed;
+  var time = Math.abs(iMag[i].image.position().left - iMag[i].xMax) * speed;
+  iMag[i].image.animate({
+    left: iMag[i].xMax
+  }, time );
+};
+function moveDown(caller) {
+  var i = $(caller).data("index");
+  var speed = (iMag[i].speed) ? iMag[i].speed : globalSpeed;
+  var time = Math.abs(iMag[i].image.position().top - iMag[i].yMax) * speed;
+  iMag[i].image.animate({
+    top: iMag[i].yMax
+  }, time );
+};
+function moveLeft(caller) {
+  var i = $(caller).data("index");
+  var speed = (iMag[i].speed) ? iMag[i].speed : globalSpeed;
+  var time = Math.abs(iMag[i].image.position().left - iMag[i].xMin) * speed;
+  iMag[i].image.animate({
+    left: iMag[i].xMin
+  }, time );
+};
+function moveUp(caller) {
+  var i = $(caller).data("index");
+  var speed = (iMag[i].speed) ? iMag[i].speed : globalSpeed;
+  var time = Math.abs(iMag[i].image.position().top - iMag[i].yMin) * speed;
+  iMag[i].image.animate({
+    top: iMag[i].yMin
+  }, time );
+};
+function stop(caller) {
+  var i = $(caller).data("index");
+  iMag[i].image.stop();
+};
+
 
 $(document).ready(function() {
   var number = $(".iMag").size();
@@ -9,7 +52,7 @@ $(document).ready(function() {
     var div = $(".iMag").eq(i);
     var image = $(".iMag").eq(i).find("img").eq(0);
     image.addClass("iMag_image");
-    iMag[i] = {};
+    if (!iMag[i]) { iMag[i] = {}; };
     iMag[i].div = div;
     iMag[i].image = image;
     iMag[i].divWidth = div.width();
@@ -41,5 +84,21 @@ $(document).ready(function() {
     iMag[i].rightControl = $('<img src="images/arrow_right.png" class="iMag_control" style="right:0px; top:'+iMag[i].controlsY+'px;" />');
     iMag[i].downControl = $('<img src="images/arrow_down.png" class="iMag_control" style="left:'+iMag[i].controlsX+'px; bottom:0px;" />');
     image.after(iMag[i].leftControl, iMag[i].upControl, iMag[i].rightControl, iMag[i].downControl);
+    iMag[i].leftControl.data("index", i).bind({
+      mousedown: function() { moveRight(this); },
+      mouseup: function() { stop(this); }
+    });
+    iMag[i].upControl.data("index", i).bind({
+      mousedown: function() { moveDown(this); },
+      mouseup: function() { stop(this); }
+    });
+    iMag[i].rightControl.data("index", i).bind({
+      mousedown: function() { moveLeft(this); },
+      mouseup: function() { stop(this); }
+    });
+    iMag[i].downControl.data("index", i).bind({
+      mousedown: function() { moveUp(this); },
+      mouseup: function() { stop(this); }
+    });
   };
 });
